80bc72ed6444016146e71515310b704cf8280998
honoka <xal2008sc@gmail.com>
2018-06-05 09:06:49 +0800

    save diary contract

--- contract/morning-diary.js
+++ contract/morning-diary.js
@@ -1,3 +1,121 @@
 /**
  * Created by honoka on 18/5/31.
  */
+"use strict"
+let DiaryItem = function (text) {
+  if (text) {
+    const obj = JSON.parse(text)
+    // this.author = obj.author
+    // this.content = obj.content
+    this.id = obj.id
+    this.date = obj.date
+    this.todo = obj.todo
+    this.learn = obj.learn
+    this.relationship = obj.relationship
+    this.health = obj.health
+    this.work = obj.work
+    this.financial = obj.financial
+    this.happiness = obj.happiness
+    this.dream = obj.dream
+    this.emoji = obj.emoji
+  } else {
+    // this.author = ''
+    this.date = ''
+    this.id = ''
+    this.todo = ''
+    this.learn = ''
+    this.relationship = ''
+    this.health = ''
+    this.work = ''
+    this.financial = ''
+    this.happiness = ''
+    this.dream = ''
+    this.emoji = ''
+  }
+}
+
+DiaryItem.prototype = {
+  toString: function () {
+    return JSON.stringify(this)
+  }
+}
+
+let MorningDiary = function () {
+  // LocalContractStorage.defineProperty(this, 'id')
+  LocalContractStorage.defineMapProperty(this, 'userList')
+  LocalContractStorage.defineMapProperty(this, 'diaryList', {
+    stringify: function (obj) {
+      return obj.toString()
+    },
+    parse: function (o) {
+      return new DiaryItem(o)
+    }
+  })
+}
+
+MorningDiary.prototype = {
+  init: function () {
+    // this.id = new BigNumber(0)
+  },
+  
+  save: function (author, date, todo, learn, relationship, health, work, financial, happiness, dream, emoji) {
+    if (!date) {
+      throw new Error('empty date')
+    }
+    author = author || Blockchain.transaction.from
+    if (!Blockchain.verifyAddress(author)) {
+      throw new Error('invalid author')
+    }
+    const id = author + date
+    const diaryItem = new DiaryItem()
+    diaryItem.id = id
+    diaryItem.date = date
+    diaryItem.todo = todo
+    diaryItem.learn = learn
+    diaryItem.relationship = relationship
+    diaryItem.health = health
+    diaryItem.work = work
+    diaryItem.financial = financial
+    diaryItem.happiness = happiness
+    diaryItem.dream = dream
+    diaryItem.emoji = emoji
+    
+    this.diaryList.put(id, diaryItem)
+    this._saveUserList(author, id)
+    return 'success'
+  },
+  
+  // _deletePreDiary: function(id, author) {
+  //   const ids = this.userList.get(author) || []
+  //   if (ids.indexOf(id) > -1) {
+  //     this.diaryList.delete(id)
+  //   }
+  // },
+  
+  _saveUserList: function(author, id) {
+    let ids = this.userList.get(author) || []
+    this.userList.put(author, ids.concat(id))
+  },
+  
+  getDiaryList: function (author) {
+    author = author || Blockchain.transaction.from
+    if (!author || !Blockchain.verifyAddress(author)) {
+      throw new Error('invalid author')
+    }
+    const ids = this.userList.get(author) || []
+    return this._getAllDiaryList(ids)
+  },
+  
+  _getAllDiaryList: function(ids) {
+    let result = []
+    if (ids && ids.length) {
+      ids.forEach(id => {
+        const list = this.diaryList.get(id)
+        result = result.concat(list)
+      })
+    }
+    return result
+  },
+}
+
+module.exports = MorningDiary
--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-
 import './App.less'
-import React, { Component } from 'react'
-import { hot } from 'react-hot-loader'
-import { Picker as EmojiPicker } from 'emoji-mart'
-import { Calendar, Card, Modal, Button, Popover, Input } from 'antd'
+import React, {Component} from 'react'
+import {hot} from 'react-hot-loader'
+import {Picker as EmojiPicker} from 'emoji-mart'
+import {Calendar, Card, Modal, Button, Popover, Input, message} from 'antd'
 import coffee from './assets/coffee.svg'
 import moment from 'moment'
 import emojione from 'emojione'
+import Nebpay from 'nebpay'
 
-const { TextArea } = Input
+const {TextArea} = Input
+const mainnetUrl = "https://pay.nebulas.io/api/mainnet/pay"
+const testnetUrl = "https://pay.nebulas.io/api/pay"
+const dappAddr = 'n1hndZE2NnWNTewjhDUrwRaaTAm7QVcDUpA'
+const nebpay = new Nebpay()
+
 class App extends Component {
   state = {
+    author: '',
     date: moment(),
     editVisible: false,
     confirmLoading: false,
+    emojiCol: '',
     emoji: '',
     todo: '', // 今日待办
     learn: '', // 学习
@@ -24,16 +31,93 @@ class App extends Component {
     happiness: '', // 快乐
     dream: '' // 梦想
   }
+  
+  componentDidMount() {
+    this.getDiaryList()
+  }
+  
   onDateSelect(date) {
     this.setState({date, editVisible: true})
   }
-  saveDiary = () => {
   
+  saveDiary = () => {
+    const {
+      date,
+      emoji,
+      todo,
+      learn,
+      relationship,
+      health,
+      work,
+      financial,
+      happiness,
+      dream,
+      author,
+    } = this.state
+    const dateStr = date.format('YYYYMMDD')
+    const callFunc = 'save'
+    const callArgs = `["${author}", "${dateStr}", "${todo}", "${learn}", "${relationship}", "${health}", "${work}", "${financial}", "${happiness}", "${dream}", "${emoji}"]`
+    message.loading('交易完成后将自动保存日记，请稍等...', 0)
+    this.serialNumber = nebpay.call(dappAddr, '0', callFunc, callArgs, {
+      callback: testnetUrl,
+      listener: res => {
+        if (res === 'Error: Transaction rejected by user') {
+          message.destroy()
+          message.error('您已拒绝交易，请重新保存')
+          clearInterval(this.queryInterval)
+        }
+      }
+    })
+    
+    this.queryInterval = setInterval(this.querySaveInfo, 10000)
   }
+  
+  querySaveInfo = () => {
+    nebpay.queryPayInfo(this.serialNumber, {callback: testnetUrl})
+      .then(resp => {
+        const result = JSON.parse(resp)
+        if (result.code === 0) {
+          clearInterval(this.queryInterval)
+          // 交易成功
+          message.destroy()
+          this.onModalCancel()
+          message.success('日记保存成功!')
+        }
+      })
+      .catch(err => {
+        clearInterval(this.queryInterval)
+        message.error('日记保存失败，请重新保存')
+        console.error(err)
+      })
+  }
+  
+  getDiaryList = () => {
+    const {
+      author,
+    } = this.state
+  
+    const callFunc = 'getDiaryList'
+    const callArgs = `["${author}"]`
+    nebpay.simulateCall(dappAddr, '0', callFunc, callArgs, {
+      callback: testnetUrl,
+      listener: function (resp) {
+        let list = JSON.parse(resp.result) || []
+        let newList = []
+        for (let i = 0; i <= list.length; i ++) {
+          const hasOccupyIndex = newList.findIndex(l => l.id === list[i].id) > -1
+          if (list[i].id === list[i - 1].id) {
+          
+          }
+        }
+      }
+    })
+  }
+  
   onModalCancel = () => {
     this.setState({
       editVisible: false,
       confirmLoading: false,
+      emojiCol: '',
       emoji: '',
       todo: '',
       learn: '',
@@ -44,11 +128,64 @@ class App extends Component {
       happiness: '',
       dream: ''
     })
+    clearInterval(this.queryInterval)
+  }
+  
+  onAboutUsShow = () => {
+    Modal.info({
+      title: '关于我们',
+      okText: '开始吧！',
+      content: (
+        <div>
+          <h2>晨间日记是什么？</h2>
+          
+          <p>晨间日记就是利用早晨的时间记录一段文字，帮助自己提高效率的工具，由佐藤传出版的<a href="https://book.douban.com/subject/3744041/">《晨间日记的奇迹》</a>中提出了该概念，通过自问自答的方式引领自己在每天早晨深入地思考与复盘。
+          </p>
+          
+          <h2>和普通日记有什么区别？</h2>
+          
+          <ol>
+            <li>冷静之后再写的大不同。经过一个晚上的时间，能更加客观的看待问题</li>
+            <li>短时间内可正确做记录。而且对前一天的事情还记忆犹新，留下正确记录</li>
+            <li>&quot;未来日记&quot;可以改变当日行动模式。写下自己的想法，让自己变得更加稳定，可以发变行动。更积极、自主地解决问题</li>
+            <li>隔天早晨再次做确认、提高执行力。隔天早上检视前一天自己所写下的未来日记，执行力也会跟着提高</li>
+            <li>有效<strong>治愈</strong>赖床！</li>
+          </ol>
+          
+          <h2>使用攻略</h2>
+          
+          <ol>
+            <li>本应用基于<a href="https://nebulas.io/">星云链</a>驱动，WEB 端使用请安装<a
+              href="https://github.com/ChengOrangeJu/WebExtensionWallet">星云钱包插件</a>，移动端使用请安装<a
+              href="https://nano.nebulas.io/index_cn.html">星云钱包 APP</a></li>
+            <li>应用已内置经典模板，帮助你快速上手晨间日记</li>
+            <li>点击相应日期即可开始书写或查看自己的晨间日记</li>
+            <li>选择一个 emoji 作为自己一天的象征吧，日历中会展示你每天变化</li>
+            <li>回忆可查看历史上的今天，你都留下了怎样的记录</li>
+          </ol>
+          
+          <h2>更多信息</h2>
+          
+          <ol>
+            <li>欢迎提交意见与反馈，可通过 <a href='mailto:xal821792703@gmail.com'>邮件</a> 或 <a href="https://github.com/nitta-honoka/morning-diary/issues">ISSUE</a> 的方式联系我们
+            </li>
+            <li>欢迎参加星云链开发者<a href="https://incentive.nebulas.io/cn/signup.html?invite=LGfSR">激励活动</a>，用一杯下午茶的时间赢取 NAS 奖励
+            </li>
+          </ol>
+          
+          <h3>现在，让我们开始享受每日清晨的奇迹时光吧！</h3>
+        </div>
+      ),
+      onOk() {
+      }
+    })
   }
   
   onEmojiPick(emoji) {
+    console.log(emoji)
     this.setState({
-      emoji: emoji.colons,
+      emojiCol: emoji.colons,
+      emoji: emoji.id
     })
   }
   
@@ -70,7 +207,7 @@ class App extends Component {
     const {
       confirmLoading,
       editVisible,
-      emoji,
+      emojiCol,
       date,
       // diary content
       todo,
@@ -86,20 +223,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={coffee} />
+          <img src={coffee}/>
           <h2>晨间日记</h2>
         </header>
         <main>
           <Card bordered={false}>
             <Calendar value={date}
-              onSelect={date => this.onDateSelect(date)}/>
+                      onSelect={date => this.onDateSelect(date)}
+                      onPanelChange={date => this.setState({date})}/>
           </Card>
           <Card title='回忆' bordered={false}>
           
           </Card>
         </main>
         <footer>
-          使用<a href='https://nebulas.io/'>星云链</a>驱动 | 查看<a>详细帮助信息</a>
+          使用<a href='https://nebulas.io/'>星云链</a>驱动 |&nbsp;<a onClick={this.onAboutUsShow}>关于我们</a>
         </footer>
         <Modal title='编写日记'
                visible={editVisible}
@@ -121,24 +259,24 @@ class App extends Component {
                 <Button type='primary'>选择一个emoji代表今天的心情吧</Button>
               </Popover>
               {
-                !!emoji && (
-                  <span dangerouslySetInnerHTML={{__html: emojione.shortnameToImage(emoji)}} />
+                !!emojiCol && (
+                  <span dangerouslySetInnerHTML={{__html: emojione.shortnameToImage(emojiCol)}} />
                 )
               }
             </div>
           </Card>
           <Card title='今天待办'
-            extra={<i>吃掉那只青蛙</i>}>
+                extra={<i>吃掉那只青蛙</i>}>
             <TextArea rows={5} value={todo}
                       onChange={e => this.onDiaryInput(e.target.value, 'todo')}/>
           </Card>
           <Card title='学习·信息·阅读'
-            extra={<i>读万卷书，行万里路</i>}>
+                extra={<i>读万卷书，行万里路</i>}>
             <TextArea rows={5} value={learn}
                       onChange={e => this.onDiaryInput(e.target.value, 'learn')}/>
           </Card>
           <Card title='人际关系·家庭·朋友'
-            extra={<i>每天做一件让人快乐的事</i>}>
+                extra={<i>每天做一件让人快乐的事</i>}>
             <TextArea rows={5} value={relationship}
                       onChange={e => this.onDiaryInput(e.target.value, 'relationship')}/>
           </Card>
