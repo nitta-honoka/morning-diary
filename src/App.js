09b07f6ef96be38156d7c75006efe609581b262e
honoka <xal2008sc@gmail.com>
2018-06-05 22:43:23 +0800

    finish front-end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.less'
 import React, {Component} from 'react'
 import {hot} from 'react-hot-loader'
 import {Picker as EmojiPicker} from 'emoji-mart'
-import {Calendar, Card, Modal, Button, Popover, Input, message} from 'antd'
+import {Calendar, Card, Modal, Button, Popover, Input, message, Alert, Select} from 'antd'
 import coffee from './assets/coffee.svg'
 import moment from 'moment'
 import emojione from 'emojione'
@@ -11,13 +11,14 @@ import Nebpay from 'nebpay'
 const {TextArea} = Input
 const mainnetUrl = "https://pay.nebulas.io/api/mainnet/pay"
 const testnetUrl = "https://pay.nebulas.io/api/pay"
-const dappAddr = 'n1hndZE2NnWNTewjhDUrwRaaTAm7QVcDUpA'
+const dappAddr = 'n1r5g2XYCw4SbyCY34VBsCi9vfk7wHiLWSJ'
 const nebpay = new Nebpay()
 
 class App extends Component {
   state = {
-    author: '',
+    diaryData: {},
     date: moment(),
+    lastYear: moment().subtract(1, 'years').year(),
     editVisible: false,
     confirmLoading: false,
     emojiCol: '',
@@ -34,10 +35,33 @@ class App extends Component {
   
   componentDidMount() {
     this.getDiaryList()
+    this.getAddress()
+  }
+  
+  getAddress() {
+    // 获得绑定钱包地址
+    window.postMessage({
+      'target': 'contentscript',
+      'data': {},
+      'method': 'getAccount'
+    }, '*')
+    
+    window.addEventListener('message', e => {
+      if (e.data && e.data.data && e.data.data.account) {
+        this.setState({address: e.data.data.account})
+      }
+    })
   }
   
   onDateSelect(date) {
-    this.setState({date, editVisible: true})
+    const { diaryData } = this.state
+    const data = diaryData[date.format('YYYYMMDD')] || {}
+    this.setState({
+      ...data,
+      emojiCol: data.emoji ? `:${data.emoji}:` : '',
+      date,
+      editVisible: true
+    })
   }
   
   saveDiary = () => {
@@ -52,11 +76,14 @@ class App extends Component {
       financial,
       happiness,
       dream,
-      author,
     } = this.state
+    
+    if (!emoji) {
+      return message.error('请选择一个 emoji 呦', 1500)
+    }
     const dateStr = date.format('YYYYMMDD')
     const callFunc = 'save'
-    const callArgs = `["${author}", "${dateStr}", "${todo}", "${learn}", "${relationship}", "${health}", "${work}", "${financial}", "${happiness}", "${dream}", "${emoji}"]`
+    const callArgs = `["", "${dateStr}", "${todo}", "${learn}", "${relationship}", "${health}", "${work}", "${financial}", "${happiness}", "${dream}", "${emoji}"]`
     message.loading('交易完成后将自动保存日记，请稍等...', 0)
     this.serialNumber = nebpay.call(dappAddr, '0', callFunc, callArgs, {
       callback: testnetUrl,
@@ -76,12 +103,13 @@ class App extends Component {
     nebpay.queryPayInfo(this.serialNumber, {callback: testnetUrl})
       .then(resp => {
         const result = JSON.parse(resp)
-        if (result.code === 0) {
+        if (result.code === 0 && result.data.status === 1) {
+          // 交易成功且打包上链
           clearInterval(this.queryInterval)
-          // 交易成功
           message.destroy()
           this.onModalCancel()
           message.success('日记保存成功!')
+          this.getDiaryList()
         }
       })
       .catch(err => {
@@ -92,25 +120,30 @@ class App extends Component {
   }
   
   getDiaryList = () => {
-    const {
-      author,
-    } = this.state
-  
     const callFunc = 'getDiaryList'
-    const callArgs = `["${author}"]`
+    const callArgs = `[]`
     nebpay.simulateCall(dappAddr, '0', callFunc, callArgs, {
       callback: testnetUrl,
-      listener: function (resp) {
+      listener: resp => {
         let list = JSON.parse(resp.result) || []
-        let newList = []
-        for (let i = 0; i <= list.length; i ++) {
-          const hasOccupyIndex = newList.findIndex(l => l.id === list[i].id) > -1
-          if (list[i].id === list[i - 1].id) {
-          
-          }
-        }
+        const data = {}
+        list.forEach(l => {
+          data[l.date] = l
+        })
+        this.setState({diaryData: data})
       }
     })
+  }
+  
+  dateCellRender(date) {
+    const { diaryData } = this.state
+    const dateStr = date.format('YYYYMMDD')
+    const data = diaryData[dateStr] || {}
+    if (data.emoji) {
+      return (
+        <span dangerouslySetInnerHTML={{__html: emojione.shortnameToImage(`:${data.emoji}:`)}} />
+      )
+    }
   }
   
   onModalCancel = () => {
@@ -133,7 +166,7 @@ class App extends Component {
   
   onAboutUsShow = () => {
     Modal.info({
-      title: '关于我们',
+      title: '帮助&反馈',
       okText: '开始吧！',
       content: (
         <div>
@@ -158,20 +191,18 @@ class App extends Component {
             <li>本应用基于<a href="https://nebulas.io/">星云链</a>驱动，WEB 端使用请安装<a
               href="https://github.com/ChengOrangeJu/WebExtensionWallet">星云钱包插件</a>，移动端使用请安装<a
               href="https://nano.nebulas.io/index_cn.html">星云钱包 APP</a></li>
-            <li>应用已内置经典模板，帮助你快速上手晨间日记</li>
-            <li>点击相应日期即可开始书写或查看自己的晨间日记</li>
-            <li>选择一个 emoji 作为自己一天的象征吧，日历中会展示你每天变化</li>
-            <li>回忆可查看历史上的今天，你都留下了怎样的记录</li>
+            <li>应用已内置经典模板，帮助您快速上手晨间日记</li>
+            <li>点击相应日期即可开始书写或查看自己写过的晨间日记</li>
+            <li>选择一个 emoji 作为自己一天的象征吧，日历会展示您每天的心情变化</li>
+            <li>可查看历史上的今天，您都留下了怎样的记录</li>
+            <li>仅保存日记需要消费少量 GAS</li>
           </ol>
           
-          <h2>更多信息</h2>
+          <h2>联系开发者</h2>
+          <p>欢迎提交意见与反馈，可通过 <a href='mailto:xal821792703@gmail.com'>邮件</a> 或 <a href="https://github.com/nitta-honoka/morning-diary/issues">ISSUE</a> 的方式联系我们</p>
           
-          <ol>
-            <li>欢迎提交意见与反馈，可通过 <a href='mailto:xal821792703@gmail.com'>邮件</a> 或 <a href="https://github.com/nitta-honoka/morning-diary/issues">ISSUE</a> 的方式联系我们
-            </li>
-            <li>欢迎参加星云链开发者<a href="https://incentive.nebulas.io/cn/signup.html?invite=LGfSR">激励活动</a>，用一杯下午茶的时间赢取 NAS 奖励
-            </li>
-          </ol>
+          <h2>星云激励计划</h2>
+          <p>欢迎参加星云链开发者<a href="https://incentive.nebulas.io/cn/signup.html?invite=LGfSR">激励活动</a>，用一杯下午茶的时间赢取 NAS 奖励</p>
           
           <h3>现在，让我们开始享受每日清晨的奇迹时光吧！</h3>
         </div>
@@ -182,7 +213,6 @@ class App extends Component {
   }
   
   onEmojiPick(emoji) {
-    console.log(emoji)
     this.setState({
       emojiCol: emoji.colons,
       emoji: emoji.id
@@ -191,6 +221,10 @@ class App extends Component {
   
   onDiaryInput = (v, key) => {
     this.setState({[key]: v})
+  }
+  
+  onMemorySelect = v => {
+    this.setState({lastYear: v})
   }
   
   get EmojiBox() {
@@ -203,21 +237,64 @@ class App extends Component {
     )
   }
   
+  get MemoryBox() {
+    const { diaryData, date, lastYear } = this.state
+    const dateStr = `${lastYear}${moment().format('MMDD')}`
+    const data = diaryData[dateStr]
+    console.log('date is %s, data is %o', dateStr, data)
+    const options = (() => {
+      let opts = []
+      for (let i = moment().subtract(1, 'years').year(); i >= 2000; i --) {
+        opts.push({label: i, value: i})
+      }
+      return opts
+    })()
+    const Title = (
+      <div>
+        <Select value={lastYear} style={{width: 80}} onChange={this.onMemorySelect}>
+          {
+            options.map(o => (
+              <Select.Option value={o.value} key={o.value}>{o.label}</Select.Option>
+            ))
+          }
+        </Select>
+        <span style={{marginLeft: '8px'}}>年的今天</span>
+      </div>
+    )
+    
+    if (data) {
+      return (
+        <Card title={Title} bordered={false} className='memory-box'>
+          {
+            TITLE_CONF.map(conf => {
+              if (data[conf.value]) {
+                return (
+                  <Card.Grid key={conf.value} style={{width: '25%'}}>
+                    <h4 className="memory-header">{conf.label}</h4>
+                    <p className="memory-content">{data[conf.value]}</p>
+                  </Card.Grid>
+                )
+              }
+            })
+          }
+        </Card>
+      )
+    } else {
+      return (
+        <Card title={Title} bordered={false} className='memory-box'>
+          <Alert message='没有这一年的回忆呦' type='info' showIcon/>
+        </Card>
+      )
+    }
+  }
+  
   render() {
     const {
       confirmLoading,
       editVisible,
+      address,
       emojiCol,
       date,
-      // diary content
-      todo,
-      learn,
-      relationship,
-      health,
-      work,
-      financial,
-      happiness,
-      dream
     } = this.state
     
     return (
@@ -227,17 +304,23 @@ class App extends Component {
           <h2>晨间日记</h2>
         </header>
         <main>
+          {
+            !!address && (
+              <Card bordered={false}>
+                <Alert message={`您当前的钱包地址为：${address || ''}`} type='info' showIcon/>
+              </Card>
+            )
+          }
           <Card bordered={false}>
             <Calendar value={date}
+                      dateCellRender={date => this.dateCellRender(date)}
                       onSelect={date => this.onDateSelect(date)}
                       onPanelChange={date => this.setState({date})}/>
           </Card>
-          <Card title='回忆' bordered={false}>
-          
-          </Card>
+          {this.MemoryBox}
         </main>
         <footer>
-          使用<a href='https://nebulas.io/'>星云链</a>驱动 |&nbsp;<a onClick={this.onAboutUsShow}>关于我们</a>
+          使用<a href='https://nebulas.io/'>星云链</a>驱动 |&nbsp;<a onClick={this.onAboutUsShow}>帮助&反馈</a>
         </footer>
         <Modal title='编写日记'
                visible={editVisible}
@@ -265,46 +348,14 @@ class App extends Component {
               }
             </div>
           </Card>
-          <Card title='今天待办'
-                extra={<i>吃掉那只青蛙</i>}>
-            <TextArea rows={5} value={todo}
-                      onChange={e => this.onDiaryInput(e.target.value, 'todo')}/>
-          </Card>
-          <Card title='学习·信息·阅读'
-                extra={<i>读万卷书，行万里路</i>}>
-            <TextArea rows={5} value={learn}
-                      onChange={e => this.onDiaryInput(e.target.value, 'learn')}/>
-          </Card>
-          <Card title='人际关系·家庭·朋友'
-                extra={<i>每天做一件让人快乐的事</i>}>
-            <TextArea rows={5} value={relationship}
-                      onChange={e => this.onDiaryInput(e.target.value, 'relationship')}/>
-          </Card>
-          <Card title='健康·饮食·锻炼'
-                extra={<i>早睡早起，劳逸结合</i>}>
-            <TextArea rows={5} value={health}
-                      onChange={e => this.onDiaryInput(e.target.value, 'health')}/>
-          </Card>
-          <Card title='工作'
-                extra={<i>行成于思，业精于勤</i>}>
-            <TextArea rows={5} value={work}
-                      onChange={e => this.onDiaryInput(e.target.value, 'work')}/>
-          </Card>
-          <Card title='理财·金钱'
-                extra={<i>金钱拥有中立的能量</i>}>
-            <TextArea rows={5} value={financial}
-                      onChange={e => this.onDiaryInput(e.target.value, 'financial')}/>
-          </Card>
-          <Card title='快乐·惊喜·其他'
-                extra={<i>真正的快乐是内在的</i>}>
-            <TextArea rows={5} value={happiness}
-                      onChange={e => this.onDiaryInput(e.target.value, 'happiness')}/>
-          </Card>
-          <Card title='愿望·人生的梦'
-                extra={<i>梦想是蝴蝶的翅膀</i>}>
-            <TextArea rows={5} value={dream}
-                      onChange={e => this.onDiaryInput(e.target.value, 'dream')}/>
-          </Card>
+          {
+            TITLE_CONF.map(conf => (
+              <Card title={conf.title} key={conf.value} extra={conf.extra}>
+                <TextArea rows={5} value={this.state[conf.value]}
+                          onChange={e => this.onDiaryInput(e.target.value, conf.value)}/>
+              </Card>
+            ))
+          }
         </Modal>
       </div>
     )
@@ -322,3 +373,37 @@ const WEEK_CONF = {
   '6': '周六',
   '7': '周日',
 }
+
+const TITLE_CONF = [{
+  label: '今日待办',
+  value: 'todo',
+  extra: <i>吃掉那只青蛙</i>
+}, {
+  label: '学习·信息·阅读',
+  value: 'learn',
+  extra: <i>读万卷书，行万里路</i>
+}, {
+  label: '人际关系·家庭·朋友',
+  value: 'relationship',
+  extra: <i>每天做一件让人快乐的事</i>
+}, {
+  label: '健康·饮食·锻炼',
+  value: 'health',
+  extra: <i>早睡早起，劳逸结合</i>
+}, {
+  label: '工作',
+  value: 'work',
+  extra: <i>行成于思，业精于勤</i>
+}, {
+  label: '理财·金钱',
+  value: 'financial',
+  extra: <i>金钱拥有中立的能量</i>
+}, {
+  label: '快乐·惊喜·其他',
+  value: 'happiness',
+  extra: <i>真正的快乐是内在的</i>
+}, {
+  label: '愿望·人生的梦',
+  value: 'dream',
+  extra: <i>梦想是蝴蝶的翅膀</i>
+}]
